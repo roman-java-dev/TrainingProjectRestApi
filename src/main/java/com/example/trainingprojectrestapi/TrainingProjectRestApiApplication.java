4b3271ed28c513ac5ba0f74dfdd3b7b2768ae3b2
Roman <romankinash09@gmail.com>
2024-04-23 16:42:14 +0300

    added project

---
+++ src/main/java/com/example/trainingprojectrestapi/TrainingProjectRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingprojectrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingProjectRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingProjectRestApiApplication.class, args);
+    }
+
+}
